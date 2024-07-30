3b2bdaede361daf2b9c38e31c9d9e96d84791d3e
Clint Cloete <oceanicpulse@Clints-MacBook-Pro.local>
2024-07-30 02:27:32 +0200

    add 'use server' on api/cron route.ts

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,4 +1,4 @@
-"use server"
+"user server"
 
 import { NextResponse } from "next/server";
 
@@ -8,7 +8,7 @@ import Product from "@/lib/models/product.model";
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 60; // This function can run for a maximum of 60 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
